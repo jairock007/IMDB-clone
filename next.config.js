359530b50897b6dcef25d506ef7315ee248e644a
jairock007 <73494601+jairock007@users.noreply.github.com>
2024-02-29 06:40:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-// next.config.js
-
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
-    domains: ["image.tmdb.org"]
-  }
+    domains: ["image.tmdb.org"],
+  },
 };
 
 module.exports = nextConfig;
